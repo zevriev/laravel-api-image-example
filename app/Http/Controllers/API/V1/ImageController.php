0c83a5e10754b9eca2c9bf25c383e8a85887e1ff
Timur Zevriev <zevriev@mail.ru>
2021-03-28 23:26:50 +0300

    resize

--- app/Http/Controllers/API/V1/ImageController.php
+++ app/Http/Controllers/API/V1/ImageController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectResource;
 use App\Models\Image;
+use App\Models\ImageThumb;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
 
 class ImageController extends Controller
 {
@@ -97,14 +99,10 @@ class ImageController extends Controller
         $errors = [];
 
         foreach ($files as $file) {
-
             $extension = $file->getClientOriginalExtension();
-
             $check = in_array($extension,$allowedfileExtension);
-
             if($check) {
                 foreach($request->fileNames as $mediaFiles) {
-
                     $path = $mediaFiles->store('public/images');
                     $name = $mediaFiles->getClientOriginalName();
 
@@ -175,9 +173,17 @@ class ImageController extends Controller
             $image_type_aux = explode("image/", $image_parts[0]);
             $image_type = $image_type_aux[1];
             $image_base64 = base64_decode($image_parts[1]);
-            $file = $this->folderPath . uniqid() . '.' . $image_type;
+
+            $fileName = uniqid() . '.' . $image_type;
+            $file = $this->folderPath . $fileName;
 
             file_put_contents(storage_path($file), $image_base64);
+
+            //store image file into directory and db
+            $save = new Image();
+            $save->title = $fileName;
+            $save->path = $this->folderPath . $fileName;
+            $save->save();
         }
     }
 
@@ -227,7 +233,8 @@ class ImageController extends Controller
             $raw = curl_exec($ch);
             list($type, $ext) = explode('/', curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
 
-            $file = $this->folderPath . uniqid() . '.' . $ext;;
+            $fileName = uniqid() . '.' . $ext;
+            $file = $this->folderPath . $fileName;
             if (file_exists($file)) {
                 unlink($file);
             }
@@ -235,6 +242,45 @@ class ImageController extends Controller
             fwrite($fp, $raw);
             fclose($fp);
             curl_close($ch);
+
+            //store image file into db
+            $image = new Image();
+            $image->title = $fileName;
+            $image->path = $file;
+            $image->save();
+
+
+            $manager = new ImageManager(array('driver' => 'imagick'));
+            $imageManager = $manager->make(storage_path($file))->resize(100, 100, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            })->resizeCanvas(100, 100);
+            $thumb100Path = storage_path($this->folderPath) . 'thumbs/' . uniqid() . '_100x100' . $ext;
+            $imageManager->save($thumb100Path);
+
+            //store image thumb file into db
+            $saveThumb = new ImageThumb();
+            $saveThumb->image_id = $image->id;
+            $saveThumb->path = $thumb100Path;
+            $saveThumb->width = 100;
+            $saveThumb->height = 100;
+            $saveThumb->save();
+
+
+            $imageManager = $manager->make(storage_path($file))->resize(400, 400, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            })->resizeCanvas(400, 400);
+            $thumb400Path = storage_path($this->folderPath) . 'thumbs/' . uniqid() . '_400x400' . $ext;
+            $imageManager->save($thumb400Path);
+
+            //store image thumb file into db
+            $saveThumb = new ImageThumb();
+            $saveThumb->image_id = $image->id;
+            $saveThumb->path = $thumb400Path;
+            $saveThumb->width = 400;
+            $saveThumb->height = 400;
+            $saveThumb->save();
         }
     }
 
