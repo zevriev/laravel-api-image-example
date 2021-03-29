46292523eb311c5665c77167982c6b340d354a64
Timur Zevriev <zevriev@mail.ru>
2021-03-29 10:43:04 +0300

    readme

--- README.md
+++ README.md
@@ -7,6 +7,16 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
 </p>
 
+##Laravel Api Image Example
+To start project 
+
+- composer install
+- php artisan serve --port=8080
+
+http://127.0.0.1:8080/api/documentation - here will swagger documentation
+
+
+
 ## About Laravel
 
 Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
--- tests/Feature/ImageTest.php
+++ tests/Feature/ImageTest.php
@@ -8,14 +8,56 @@ use Tests\TestCase;
 class ImageTest extends TestCase
 {
     /**
-     * A image test.
+     * An image list test.
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    public function indexTest() {
+        $response = $this->get('/api/images');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+                ->assertJson([
+                    'message' => 'Retrieved successfully',
+                ]);
+    }
+
+    /**
+     * An image upload multipart test
+     * @return void
+     */
+    public function storeTest() {
+
+    }
+
+    /**
+     * An image save from base64 test
+     * @return void
+     */
+    public function storeBase64Test() {
+
+    }
+
+    /**
+     * An image download from url test
+     * @return void
+     */
+    public function imagesFromUrlTest() {
+
+    }
+
+    /**
+     * An image show test
+     * @return void
+     */
+    public function showTest() {
+
+    }
+
+    /**
+     * An image deleting test
+     * @return void
+     */
+    public function destroyTest() {
+
     }
 }
